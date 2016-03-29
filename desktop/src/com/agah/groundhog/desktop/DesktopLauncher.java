fe77b1680abd1f6ada74454b1c5becd411968e43
Alden Hart <alden.hart9@gmail.com>
2016-03-29 19:40:32 -0400

    Create project with LibGDX

---
+++ desktop/src/com/agah/groundhog/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.agah.groundhog.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.agah.groundhog.GroundhogDayGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GroundhogDayGame(), config);
+	}
+}
